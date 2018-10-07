785ceabf46fc79efd8e4c28fc11933fa14053876
wata <w.nagasawa@gmail.com>
2018-10-08 01:24:42 +0900

    Remove jquery

--- src/popup.ts
+++ src/popup.ts
@@ -1,17 +1,33 @@
-import * as $ from 'jquery'
+document.addEventListener('DOMContentLoaded', async () => {
+  const imageURLs = await fetchImageURLs()
+  for (const imagePromise of imageURLs.map(loadImage)) {
+    const img = await imagePromise
+    document
+      .getElementById('content')
+      .appendChild(document.createElement('li'))
+      .appendChild(img)
+  }
+}, false)
 
-$(document).ready(() => {
-  $.getJSON('https://api.giphy.com/v1/gifs/trending?limit=3&api_key=dc6zaTOxFJmzC&offset=' + Math.floor(Math.random() * 1000), (data) => {
-    $('img').each((i, el) => {
-      $(el).attr('src', data.data[i].images.fixed_width.url)
-      $(el).unbind().click(() => {
-        var text = "![LGTM](" + $(el).attr('src') + ")"
-        var clipboard = $('<input/>')
-        $('body').append(clipboard)
-        clipboard.val(text).select()
-        document.execCommand('copy')
-        clipboard.remove()
-      })
-    })
+async function fetchImageURLs(): Promise<[string]> {
+  const url = 'https://api.giphy.com/v1/gifs/trending?limit=3&api_key=dc6zaTOxFJmzC&offset=' + Math.floor(Math.random() * 1000)
+  const res = await fetch(url)
+  const json = await res.json()
+  return json.data.map(item => item.images.fixed_width.url)
+}
+
+function loadImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image = new Image(200)
+    image.onload = () => resolve(image)
+    image.onerror = () => reject()
+    image.onclick = () => {
+      const input = document.createElement('input')
+      input.value = `![LGTM](${url})`
+      document.body.appendChild(input).select()
+      document.execCommand('copy')
+      document.body.removeChild(input)
+    }
+    image.src = url
   })
-})
+}
