e54dd2c5dec087b7a499ab40d81594255137f180
wata <me@wata.dev>
2021-08-13 18:59:31 +0900

    Add safari

---
+++ safari/LGTM/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  LGTM
+//
+//  Created by Wataru Nagasawa on 2021/08/13.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
